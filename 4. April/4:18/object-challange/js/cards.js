bad542614fdf07255adb2e883bc071f763000780
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-04-25 14:15:19 +0800

    4/25 update

--- 4. April/4:18/object-challange/js/cards.js
+++ 4. April/4:18/object-challange/js/cards.js
@@ -1,144 +1,76 @@
-let foods = [
-  {
-    name: "Зайрмаг",
-    type: "desert",
-    price: "4,760₮",
-    photo: `img/onSaleImg/icecream.png`,
-  },
-  {
-    name: "Панкейк",
-    type: "desert",
-    price: "4,800₮",
-    photo: `img/onSaleImg/pancake.png`,
-  },
-  {
-    name: "Панкейк",
-    type: "desert",
-    price: "4,800₮",
-    photo: `img/onSaleImg/pancake.png`,
-  },
-  {
-    name: "Панкейк",
-    type: "desert",
-    price: "4,800₮",
-    photo: `img/onSaleImg/pancake.png`,
-  },
-  {
-    name: "Салмон загас",
-    type: "mainDish",
-    price: "11,800₮",
-    photo: `img/mainDishImg/Salmon.png`,
-  },
-  {
-    name: "Бөөрөнхий мах",
-    type: "mainDish",
-    price: "11,800₮",
-    photo: `img/mainDishImg/meatball.png`,
-  },
-  {
-    name: "Бөөрөнхий мах",
-    type: "mainDish",
-    price: "11,800₮",
-    photo: `img/mainDishImg/meatball.png`,
-  },
-  {
-    name: "Бөөрөнхий мах",
-    type: "mainDish",
-    price: "11,800₮",
-    photo: `img/mainDishImg/meatball.png`,
-  },
-  {
-    name: "Детокс салад",
-    type: "sideDish",
-    price: "6,800₮",
-    photo: `img/sideDishImg/detoxSalad.png`,
-  },
-  {
-    name: "Кобб салад",
-    type: "sideDish",
-    price: "6,800₮",
-    photo: `img/sideDishImg/kobbSalad.png`,
-  },
-  {
-    name: "Кобб салад",
-    type: "sideDish",
-    price: "6,800₮",
-    photo: `img/sideDishImg/kobbSalad.png`,
-  },
-  {
-    name: "Кобб салад",
-    type: "sideDish",
-    price: "6,800₮",
-    photo: `img/sideDishImg/kobbSalad.png`,
-  },
-  {
-    name: "Өглөөний хоол",
-    type: "onSale",
-    price: "6,800₮",
-    photo: `img/onSaleImg/breakfast.png`,
-  },
-  {
-    name: "Банана сендвич",
-    type: "onSale",
-    price: "6,800₮",
-    photo: `img/onSaleImg/bananaSandwich.png`,
-  },
-  {
-    name: "Банана сендвич",
-    type: "onSale",
-    price: "6,800₮",
-    photo: `img/onSaleImg/bananaSandwich.png`,
-  },
-  {
-    name: "Банана сендвич",
-    type: "onSale",
-    price: "6,800₮",
-    photo: `img/onSaleImg/bananaSandwich.png`,
-  },
-];
-
-let onSale = ``;
-let desert = ``;
-let sideDish = ``;
-let mainDish = ``;
-
-for (i = 0; i < foods.length; i++) {
-  if (foods[i].type == "desert") {
-    desert += ` 
-      <div class = "cardInfo col-3 p-3">
-        <img src="${foods[i].photo}" class="w-100">
-        <h3>${foods[i].name}</h3>
-        <p>${foods[i].price}</p>
-      </div>
-  `;
-  } else if (foods[i].type == "mainDish") {
-    mainDish += `
-    <div class = "cardInfo col-3 p-3">
-      <img src="${foods[i].photo}" class="w-100">
-      <h3>${foods[i].name}</h3>
-      <p>${foods[i].price}</p>
-    </div>
-  `;
-  } else if (foods[i].type == "sideDish") {
-    sideDish += `
-    
-    <div class = "cardInfo col-3 p-3">
-      <img src="${foods[i].photo}" class="w-100">
-      <h3>${foods[i].name}</h3>
-      <p>${foods[i].price}</p>
-    </div>
-  `;
-  } else if (foods[i].type == "onSale") {
-    onSale += `
-    <div class = "cardInfo col-3 p-3">
-      <img src="${foods[i].photo}" class="w-100">
-      <h3>${foods[i].name}</h3>
-      <p>${foods[i].price}</p>
-    </div>
-  `;
+const createFood = (arr, container) => {
+  let html = document.querySelector(container);
+  for (i = 0; i < arr.length; i++) {
+    let food = `
+        <div class = "cardInfo col-3 p-3">
+          <img src="${arr[i].photo}" class="w-100">
+          <h3>${arr[i].name}</h3>
+          <div class="d-flex justify-content-between">
+            <p>${arr[i].price}</p>
+            <p>saleprice</p>
+          <div> 
+        </div>`;
+    html.innerHTML += food;
   }
-}
-document.querySelector("#cardsOnSale").innerHTML = onSale;
-document.querySelector("#cardsDesert").innerHTML += desert;
-document.querySelector("#cardsSideDish").innerHTML += sideDish;
-document.querySelector("#cardsMainDish").innerHTML += mainDish;
+};
+
+const xhrFood = new XMLHttpRequest();
+xhrFood.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    const datas = JSON.parse(this.responseText);
+
+    const onSaleArr = datas.filter((food) => food.sale > 0);
+    const desertArr = datas.filter((food) => food.type == "desert");
+    const sideDishArr = datas.filter((food) => food.type == "sideDish");
+    const mainDishArr = datas.filter((food) => food.type == "mainDish");
+
+    createFood(onSaleArr, "#cardsOnSale");
+    createFood(desertArr, "#cardsDesert");
+    createFood(sideDishArr, "#cardsSideDish");
+    createFood(mainDishArr, "#cardsMainDish");
+  }
+};
+
+xhrFood.open("GET", "/js/data.json", true);
+xhrFood.send();
+
+// for (i = 0; i < foods.length; i++) {
+//   if (foods[i].type == "desert") {
+//     desert += `
+//       <div class = "cardInfo col-3 p-3">
+//         <img src="${foods[i].photo}" class="w-100">
+//         <h3>${foods[i].name}</h3>
+//         <p>${foods[i].price}</p>
+//       </div>
+//   `;
+//   } else if (foods[i].type == "mainDish") {
+//     mainDish += `
+//     <div class = "cardInfo col-3 p-3">
+//       <img src="${foods[i].photo}" class="w-100">
+//       <h3>${foods[i].name}</h3>
+//       <p>${foods[i].price}</p>
+//     </div>
+//   `;
+//   } else if (foods[i].type == "sideDish") {
+//     sideDish += `
+
+//     <div class = "cardInfo col-3 p-3">
+//       <img src="${foods[i].photo}" class="w-100">
+//       <h3>${foods[i].name}</h3>
+//       <p>${foods[i].price}</p>
+//     </div>
+//   `;
+//   } else if (foods[i].type == "onSale") {
+//     onSale += `
+//     <div class = "cardInfo col-3 p-3">
+//       <img src="${foods[i].photo}" class="w-100">
+//       <h3>${foods[i].name}</h3>
+//       <p>${foods[i].price}</p>
+//     </div>
+//   `;
+//   }
+// }
+// document.querySelector("#cardsOnSale").innerHTML = onSale;
+// document.querySelector("#cardsDesert").innerHTML += desert;
+// document.querySelector("#cardsSideDish").innerHTML += sideDish;
+// document.querySelector("#cardsMainDish").innerHTML += mainDish;
